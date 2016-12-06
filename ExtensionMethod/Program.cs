66c80ed80c45aa549d7f7762bfa83293b62f231f
waliul-cse <waliul.cse2010@gmail.com>
2016-12-06 18:32:03 +0600

    added some files

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = x.Add(10);
+            Console.WriteLine(y);
+
+            //extension method without parameter
+            int hi = 100;
+            int result = hi.Hello();
+            Console.WriteLine(result);
+
+            //test overload method
+            int z = x.Add(100, 50);
+            Console.WriteLine(z);
+
+            //More use of extension method
+            int s = 100;
+            int res = s.Square().Double().Add(5);
+            Console.WriteLine(res);
+
+            Console.ReadLine();
+        }
+    }
+}
