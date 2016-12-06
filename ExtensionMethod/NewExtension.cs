940cb0c62b16b1f4072f707dc39a8e5a6c9ed542
waliul-cse <waliul.cse2010@gmail.com>
2016-12-06 18:28:30 +0600

    ExtensionMethod Example code in simple console application

---
+++ ExtensionMethod/NewExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class NewExtension
+    {
+        //Extension method must be static and need to use "this" before first parameter
+        public static int Add(this int a, int b)
+        {
+            return a + b;
+
+
+        }
+
+        //Overloading method example
+        public static int Add(this int a, int b, int c)
+        {
+            return a + b + c;
+
+
+        }
+
+        public static int Hello(this int source)
+        {
+
+            return source / 2;
+        }
+
+        public static int Double(this int source)
+        {
+
+            return source * 2;
+        }
+
+        public static int Square(this int source)
+        {
+
+            return source * source;
+        }
+    }
+}
